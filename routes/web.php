47d24bea8ea91f7d2583c66df74d02eb847d5f5b
robykartis <robykarti.s@gmail.com>
2022-12-24 04:54:58 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
+
+// Route::controller(RoleController::class)->group(function () {
+//     Route::get('role', 'index')->middleware('can:read role');
+//     Route::get('role/create', 'create')->middleware('can:create role');
+// });
+
+Route::resource('role', RoleController::class);
